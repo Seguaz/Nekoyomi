ba2601005e642aa38462a31a282c1f218b715a02
Seguaz <142988911+Seguaz@users.noreply.github.com>
2026-08-15 01:30:47 +0200

    Nekoyomi v0.19.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "app.nekoyomi"
 
-        versionCode = 144
-        versionName = "0.19.6"
+        versionCode = 145
+        versionName = "0.19.7"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
